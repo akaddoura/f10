1110d4e0bcedad6ea4a345201da3291d9894942f
Ahmad Kaddoura <ahmadmkaddoura@gmail.com>
2023-04-12 02:56:13 -0400

    restaring

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:f10/editor.dart';
+import 'package:f10/textdocument.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ lib/textdocument.dart
@@ -0,0 +1,4 @@
+import 'package:collection/collection.dart';
+import 'package:flutter/widgets.dart';
+
+class Editor extends StatefulWidget {}
