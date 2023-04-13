385e49599740da767f2d4697d9645ff1f602b951
Ahmad Kaddoura <ahmad.k@radskunkworks.com>
2023-04-13 02:00:53 -0400

    fixed build errors

--- lib/textdocument.dart
+++ lib/textdocument.dart
@@ -1,4 +1,27 @@
 import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Editor extends StatefulWidget {}
+class Editor extends StatefulWidget {
+  const Editor({Key? key}) : super(key: key);
+
+  // @override
+  // _EditorState createState() = _EditorState();
+
+  @override
+  State<Editor> createState() => _EditorState();
+}
+
+class _EditorState extends State<Editor> {
+  late String _text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Call me Ishmael."),
+      ),
+      body: Container(),
+    );
+  }
+}
